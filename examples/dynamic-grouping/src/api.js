eeb62e5d4475e0d93a2e56825bb6477a1192f21d
Anand Gorantala <ianand2@gmail.com>
2024-05-14 22:51:03 -0700

    task: add dynamic grouping example (#6)

---
+++ examples/dynamic-grouping/src/api.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { account_id, auth_key, domain_key } from "./config";
+
+export const getSearchResults = (query, page, perPage, groupBy) => {
+  return axios
+    .get(constructSearchUrl(query, page, perPage, groupBy))
+    .then((response) => {
+      return response.data;
+    });
+};
+
+const constructSearchUrl = (query, page, perPage, groupBy) => {
+  const uid = encodeURIComponent(`uid=12345:v=11.8:ts=${Date.now()}:hc=3`);
+
+  const params = new URLSearchParams({
+    _br_uid_2: uid,
+    account_id: account_id,
+    auth_key: auth_key,
+    domain_key: domain_key,
+    search_type: "keyword",
+    request_type: "search",
+    url: "https://example.com",
+    ref_url: "https://example.com",
+    request_id: "12345",
+    "facet.version": "3.0",
+    q: query,
+    groupby: groupBy,
+    group_limit: 8,
+    start: page * perPage,
+    rows: perPage,
+    "stats.field": "sale_price",
+    "query.numeric_precision": "standard",
+    br_diagnostic: "all",
+    fl: "pid,score,is_live,title,description,brand,price,price_range,sale_price,sale_price_range,url,promotions,thumb_image,skuid,sku_color,sku_size,sku_thumb_images,sku_swatch_images,sku_price,sku_sale_price,onSale,inStock",
+  });
+
+  return `https://core.dxpapi.com/api/v1/core/?${params.toString()}`;
+};
