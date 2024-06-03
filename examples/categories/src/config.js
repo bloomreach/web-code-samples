0b0a124758cf6e50bbbf4ffa7044683a54a0b8be
Anand Gorantala <ianand2@gmail.com>
2024-06-03 14:19:32 -0700

    chore: Switch the categories example to use the new showcase account

--- examples/categories/src/config.js
+++ examples/categories/src/config.js
@@ -1,4 +1,4 @@
-export const account_id = "6413";
-export const auth_key = "bcvpynhij980k0y1";
-export const domain_key = "pacifichome";
-export const account_name = "Pacific Home";
+export const account_id = "7634";
+export const auth_key = "zjlc0tsp2xu7l7ro";
+export const domain_key = "showcase_pacifichome";
+export const account_name = "showcase-pacifichome.bloomreach.io";
