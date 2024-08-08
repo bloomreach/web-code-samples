56c85ab3a5fdc4f3fb3eecd0186d0822a14653e8
Anand Gorantala <ianand2@gmail.com>
2024-08-08 13:01:05 +0900

    task: Use the discovery-web-sdk in the facets example (#20)
    Co-authored-by: Anand Gorantala <anand.gorantala@bloomreach.com>

--- examples/facets/src/api.js
+++ examples/facets/src/api.js
@@ -1,19 +1,10 @@
-import axios from "axios";
 import { account_id, auth_key, domain_key } from "./config";
+import { productSearch } from '@bloomreach/discovery-web-sdk';
 
-export const getSearchResults = (
-  query,
-  page,
-  perPage,
-  sort,
-  selectedFacets,
-  facets,
-) => {
-  return axios
-    .get(constructSearchUrl(query, page, perPage, sort, selectedFacets, facets))
-    .then((response) => {
-      return response.data;
-    });
+const config = {
+  account_id,
+  auth_key,
+  domain_key,
 };
 
 function formatFilterQuery(value) {
@@ -28,23 +19,17 @@ function escapeFilterQuery(value) {
   return value.split('"').join('\\"');
 }
 
-const constructSearchUrl = (
+export const getSearchResults = (
   query,
   page,
   perPage,
   sort,
   selectedFacets,
-  facets,
 ) => {
   const uid = encodeURIComponent(`uid=12345:v=11.8:ts=${Date.now()}:hc=3`);
 
-  // See https://documentation.bloomreach.com/discovery/reference/product-search-category-api
-  // for descriptions about the parameters used below
-  const params = new URLSearchParams({
+  const params = {
     _br_uid_2: uid,
-    account_id: account_id,
-    auth_key: auth_key,
-    domain_key: domain_key,
     search_type: "keyword",
     request_type: "search",
     url: "https://example.com",
@@ -58,14 +43,9 @@ const constructSearchUrl = (
     "stats.field": "sale_price",
     "query.numeric_precision": "standard",
     br_diagnostic: "all",
+    fq: Object.keys(selectedFacets).map(key => `${key}:${formatFilterQuery(selectedFacets[key])}`),
     fl: "pid,score,is_live,title,description,brand,price,price_range,sale_price,sale_price_range,url,promotions,thumb_image,skuid,sku_color,sku_size,sku_thumb_images,sku_swatch_images,sku_price,sku_sale_price,onSale,inStock",
-  });
+  };
 
-  Object.keys(selectedFacets).forEach((key) => {
-    if (selectedFacets[key]) {
-      params.append("fq", `${key}:${formatFilterQuery(selectedFacets[key])}`);
-    }
-  });
-
-  return `https://core.dxpapi.com/api/v1/core/?${params.toString()}`;
+  return productSearch(config, params);
 };
