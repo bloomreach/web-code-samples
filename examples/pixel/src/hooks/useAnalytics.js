76c33a42cabc4dee7a1b9bbc9692428132fb29a2
Anand Gorantala <anand.gorantala@bloomreach.com>
2024-10-09 12:46:35 -0700

    fix: Implement feedback on pixel analytics instrumentation

--- examples/pixel/src/hooks/useAnalytics.js
+++ examples/pixel/src/hooks/useAnalytics.js
@@ -27,7 +27,7 @@ function useAnalytics() {
         acct_id: account_id,
         domain_key,
         user_id: userId,
-        orig_ref_url: `${window.location.origin}${pathname}`,
+        explicit_referrer: window.__BR_PRIOR_REFERRER__ ? window.__BR_PRIOR_REFERRER__ : document.referrer,
       },
       ...data,
     };
@@ -44,8 +44,12 @@ function useAnalytics() {
           prod_name: data.title,
           sku: data.sku,
         };
+        // for initial load, when the tracker script has not loaded, this will ensure that the view event
+        // fires after the tracker script loads
+        window.br_data = constructPayload(payload);
         window.BrTrk?.getTracker().updateBrData(constructPayload(payload));
         window.BrTrk?.getTracker().logPageView();
+        window.__BR_PRIOR_REFERRER__ = window.location.href;
         break;
       case 'view_category':
         payload = {
@@ -53,18 +57,24 @@ function useAnalytics() {
           cat_id: data.cat_id,
           cat: data.cat_crumb,
         };
+        // for initial load, when the tracker script has not loaded, this will ensure that the view event
+        // fires after the tracker script loads
+        window.br_data = constructPayload(payload);
         window.BrTrk?.getTracker().updateBrData(constructPayload(payload));
         window.BrTrk?.getTracker().logPageView();
-
+        window.__BR_PRIOR_REFERRER__ = window.location.href;
         break;
       case 'view_search':
         payload = {
           ptype: 'search',
           search_term: data.query,
         };
-
+        // for initial load, when the tracker script has not loaded, this will ensure that the view event
+        // fires after the tracker script loads
+        window.br_data = constructPayload(payload);
         window.BrTrk?.getTracker().updateBrData(constructPayload(payload));
         window.BrTrk?.getTracker().logPageView();
+        window.__BR_PRIOR_REFERRER__ = window.location.href;
         break;
       case 'view_conversion':
         payload = {
@@ -134,15 +144,17 @@ function useAnalytics() {
         payload = {
           wid: data.widgetId,
           wty: data.widgetType,
+          wq: data.widgetQuery, // Applicable for query widgets like search, category, past_purchases widgets
           wrid: data.widgetRequestId,
         };
 
-        window.BrTrk?.getTracker().logEvent('widget', 'widget-view', constructPayload(payload), true);
+        window.BrTrk?.getTracker().logEvent('widget', 'widget-view', constructPayload(payload));
         break;
       case 'event_widgetClick':
         payload = {
           wid: data.widgetId,
           wty: data.widgetType,
+          wq: data.widgetQuery, // Applicable for query widgets like search, category, past_purchases widgets
           wrid: data.widgetRequestId,
           item_id: data.itemId,
         };
